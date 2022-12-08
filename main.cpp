fc3cfadaa9e47764563bb0afd232eb2821f6c174
khosta77 <puwerfulpants@gmail.com>
2022-12-08 19:43:39 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main([[ maybe_unused ]] int argc, [[ maybe_unused ]] const char** argv) {
+    cout << "Hello, world!" << endl;
+    return 0;
+}
