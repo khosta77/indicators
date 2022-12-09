cb82ce933dafaa765f378b6646a3773de3aebe34
khosta77 <puwerfulpants@gmail.com>
2022-12-09 23:31:17 +0300

    Почистил индикаторы и немного их переделал. Теперь они возращают пару, код со временем и сам массив, в полном виде, то есть значения которые не попадают в диапазон равну 0

---
+++ indicator.cpp
@@ -0,0 +1,21 @@
+#include "indicator.h"
+
+namespace SMA {
+
+    const std::pair<std::string, std::vector<double>> sma(const std::vector<double>& stock, const int& day) {
+        if ((stock.size() - 1) < day)
+            return std::pair(("SMA" + std::to_string(day)), std::vector<double>(0));
+        else if ((stock.size() - 1) == day) {
+            double price = std::accumulate(stock.begin(), stock.end(), double(0));
+            std::vector<double> sma;
+            sma.push_back(price / day);
+            return std::pair(("SMA" + std::to_string(day), sma));
+        }
+
+        std::vector<double> sma(day, 0);
+        for (auto it = (stock.begin() + day); it != stock.end(); ++it)
+            sma.push_back((std::accumulate((it - day), it, double(0)) / double(day)));
+        return std::pair(("SMA" + std::to_string(day)), sma);
+    }
+
+};  // SMA
--- indicator.h
+++ indicator.h
@@ -15,21 +15,8 @@
 
 namespace SMA {
 
-    std::vector<double> sma(const std::vector<double>& stock, const int& day) {
-        if ((stock.size() - 1) < day)
-            return std::vector<double>(0);
-        else if ((stock.size() - 1) == day) {
-            double price = std::accumulate(stock.begin(), stock.end(), double(0));
-            std::vector<double> sma;
-            sma.push_back(price / day);
-            return sma;
-        }
+    const std::pair<std::string, std::vector<double>> sma(const std::vector<double>& stock, const int& day);
 
-        std::vector<double> sma(day, 0);
-        for (auto it = (stock.begin() + day); it != stock.end(); ++it)
-            sma.push_back((std::accumulate((it - day), it, double(0)) / double(day)));
-        return sma;
-    }
 };  // SMA
 
 #endif // INDICATOR_INDICATOR_H_
