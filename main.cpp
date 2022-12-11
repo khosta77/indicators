73373ad7ad853ff9e7e90f64298906c8eb182f56
khosta77 <puwerfulpants@gmail.com>
2022-12-11 20:28:21 +0300

    вывоз меню

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include "candle.h"
 #include "utilites.h"
-#include "indicator.h"
 
 using namespace std;
 
 
 int main([[ maybe_unused ]] int argc, [[ maybe_unused ]] const char** argv) {
-	[[ maybe_unused ]] auto cndls = readcsv("./GAZP.csv");
-    [[ maybe_unused ]] auto open = get_price_stock(cndls, OPEN);
-    [[ maybe_unused ]] auto sma30 = SMA::sma(open, 30);
-    cout << "MySMA" << int(30) << endl;
-    for (auto it : sma30)
-        cout << it << endl;
+    Menu mn;
+    mn.run();
+//	[[ maybe_unused ]] auto cndls = readcsv("./GAZP.csv");
+//  [[ maybe_unused ]] auto open = get_price_stock(cndls, OPEN);
+//  [[ maybe_unused ]] auto sma30 = SMA::sma(open, 30);
+//  cout << "MySMA" << int(30) << endl;
+//  for (auto it : sma30)
+//      cout << it << endl;
 //    outCandle(cndls);
 //	for (auto it : cndls)
 //		cout << it.date << ";" << it.time << ";" << it.open << ";" << it.close << endl;
